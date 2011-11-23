aee41fef01d3a2ad249b931a3af0cd1acec91faa
Lars Solberg <lars.solberg+git@gmail.com>
2011-11-23 08:12:36 +0100

    Added help(), and test() idea

--- challenges/dummy.py
+++ challenges/dummy.py
@@ -2,6 +2,9 @@
 class Challenge:
     description = 'Dummy challenge which outputs the solution'
 
+    def help(self):
+        return 'This is just a dummy challenge which does nothing. The stdout stream is the solution..'
+
     def generate(self):
         return 'dummy'
 
